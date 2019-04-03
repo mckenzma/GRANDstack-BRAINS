b5f49d5e4529119da8e42f361c26e5e447628f8c
Michael McKenzie <mckenzma@gmail.com>
2019-04-03 10:33:34 -0400

    delete commented out code

--- ui/src/BridgeList.js
+++ ui/src/BridgeList.js
@@ -3,21 +3,11 @@ import { Query } from "react-apollo";
 import gql from "graphql-tag";
 import "./BridgeList.css";
 import { withStyles } from "@material-ui/core/styles";
-// import Table from "@material-ui/core/Table";
-// import TableBody from "@material-ui/core/TableBody";
-// import TableCell from "@material-ui/core/TableCell";
-// import TableHead from "@material-ui/core/TableHead";
-// import TableRow from "@material-ui/core/TableRow";
-// import Tooltip from "@material-ui/core/Tooltip";
-import Paper from "@material-ui/core/Paper";
-// import { TableSortLabel } from "@material-ui/core";
 
-// import Map from './Map';
+import Paper from "@material-ui/core/Paper";
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 import MapLeaf from './MapLeaflet';
-
-// import TemporaryDrawer from './BridgeDrawer';
 
 const styles = theme => ({
   root: {
@@ -31,20 +21,13 @@ const styles = theme => ({
   }
 });
 
-// function getSorting(order, orderBy) {
-  // return order === "desc"
-    // ? (a, b) => (b[orderBy] < a[orderBy] ? -1 : 1)
-    // : (a, b) => (a[orderBy] < b[orderBy] ? -1 : 1);
-// }
 
 class BridgeList extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      // order: "asc",
-      // orderBy: "avgStars",
-      
+           
       businesses: [],
       mapCenter: {
         longitude: -93.2650,
@@ -54,17 +37,7 @@ class BridgeList extends React.Component {
     };
   }
 
-  // handleSortRequest = property => {
-  //   const orderBy = property;
-  //   let order = "desc";
-
-  //   if (this.state.orderBy === property && this.state.order === "desc") {
-  //     order = "asc";
-  //   }
-
-  //   this.setState({ order, orderBy });
-  // };
-
+  
   render() {
     // const { order, orderBy } = this.state;
     return (
@@ -85,94 +58,7 @@ class BridgeList extends React.Component {
 
           return (
             <Paper className={this.props.classes.root}>
-              {/*<Map
-                businesses={data.Bridge}
-              />*/}
-
-              <MapLeaf
-                // businesses={data.Bridge}
-              />
-              
-              {/*<TemporaryDrawer />*/}
-
-              {/*<Table className={this.props.classes.table}>
-                <TableHead>
-                  <TableRow>
-                    <TableCell
-                      key="id"
-                      sortDirection={orderBy === "id" ? order : false}
-                    >
-                      <Tooltip
-                        title="Sort"
-                        placement="bottom-start"
-                        enterDelay={300}
-                      >
-                        <TableSortLabel
-                          active={orderBy === "id"}
-                          direction={order}
-                          onClick={() => this.handleSortRequest("id")}
-                        >
-                          Id
-                        </TableSortLabel>
-                      </Tooltip>
-                    </TableCell>
-                    <TableCell
-                      key="latitude_decimal"
-                      sortDirection={orderBy === "latitude_decimal" ? order : false}
-                      numeric
-                    >
-                      <Tooltip
-                        title="Sort"
-                        placement="bottom-end"
-                        enterDelay={300}
-                      >
-                        <TableSortLabel
-                          active={orderBy === "latitude_decimal"}
-                          direction={order}
-                          onClick={() => this.handleSortRequest("latitude_decimal")}
-                        >
-                          Latitude
-                        </TableSortLabel>
-                      </Tooltip>
-                    </TableCell>
-                    <TableCell
-                      key="longitude_decimal"
-                      sortDirection={orderBy === "longitude_decimal" ? order : false}
-                      numeric
-                    >
-                      <Tooltip
-                        title="Sort"
-                        placement="bottom-end"
-                        enterDelay={300}
-                      >
-                        <TableSortLabel
-                          active={orderBy === "longitude_decimal"}
-                          direction={order}
-                          onClick={() => this.handleSortRequest("longitude_decimal")}
-                        >
-                          Longitude
-                        </TableSortLabel>
-                      </Tooltip>
-                    </TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {data.Bridge
-                    .slice()
-                    .sort(getSorting(order, orderBy))
-                    .map(n => {
-                      return (
-                        <TableRow key={n.id}>
-                          <TableCell component="th" scope="row">
-                            {n.id}
-                          </TableCell>
-                          <TableCell numeric>{n.latitude_decimal}</TableCell>
-                          <TableCell numeric>{n.longitude_decimal}</TableCell>
-                        </TableRow>
-                      );
-                    })}
-                </TableBody>
-              </Table>*/}
+              <MapLeaf />
             </Paper>
           );
         }}
