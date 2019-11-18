2568720fb58811320585d6519f1c857c523c56ca
Michael McKenzie <mckenzma@gmail.com>
2019-11-18 13:27:02 -0500

    remove old commented out labels and data

--- ui/src/BridgeRadar.js
+++ ui/src/BridgeRadar.js
@@ -17,7 +17,7 @@ class BridgeChart extends React.Component {
       "APPR_ROAD_EVAL_072"
     ].forEach(key => {
       if (this.props[key] !== "N") {
-        labels.push(key.substr(-3));
+        labels.push(key.substr(-3)); //use .substr to extract last 3 of labels. need to update
         data.push(this.props[key]);
       }
     });
@@ -25,20 +25,6 @@ class BridgeChart extends React.Component {
     this.state = {
       options: {
         labels,
-        // labels: [
-        //   // "STRUCTURAL_EVAL_067",
-        //   "067",
-        //   // "DECK_GEOMETRY_EVAL_068",
-        //   "068",
-        //   // "UNDCLRENCE_EVAL_069",
-        //   "069",
-        //   // "POSTING_EVAL_070",
-        //   "070",
-        //   // "WATERWAY_EVAL_071",
-        //   "071",
-        //   // "APPR_ROAD_EVAL_072"
-        //   "072"
-        // ],
         title: {
           text: "Bridge Component Rating"
         },
@@ -75,39 +61,6 @@ class BridgeChart extends React.Component {
           // TODO - FIX "5" showing up twice?
           name: "Inspection Log",
           data
-          // data: [
-          //   // TODO: Clean this logic up. 0 is not an accurate way to handle this...
-          //   parseInt(
-          //     this.props.STRUCTURAL_EVAL_067 == "N"
-          //       ? "0"
-          //       : this.props.STRUCTURAL_EVAL_067
-          //   ),
-          //   parseInt(
-          //     this.props.DECK_GEOMETRY_EVAL_068 == "N"
-          //       ? "0"
-          //       : this.props.DECK_GEOMETRY_EVAL_068
-          //   ),
-          //   parseInt(
-          //     this.props.UNDCLRENCE_EVAL_069 == "N"
-          //       ? "0"
-          //       : this.props.UNDCLRENCE_EVAL_069
-          //   ),
-          //   parseInt(
-          //     this.props.POSTING_EVAL_070 == "N"
-          //       ? "0"
-          //       : this.props.POSTING_EVAL_070
-          //   ),
-          //   parseInt(
-          //     this.props.WATERWAY_EVAL_071 == "N"
-          //       ? "0"
-          //       : this.props.WATERWAY_EVAL_071
-          //   ),
-          //   parseInt(
-          //     this.props.APPR_ROAD_EVAL_072 == "N"
-          //       ? "0"
-          //       : this.props.APPR_ROAD_EVAL_072
-          //   )
-          // ]
         }
       ]
     };
@@ -129,6 +82,3 @@ class BridgeChart extends React.Component {
 }
 
 export default BridgeChart;
-
-// const domContainer = document.querySelector('#app');
-// ReactDOM.render(React.createElement(BridgeChart), domContainer);
