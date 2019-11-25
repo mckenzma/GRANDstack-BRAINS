140275fa9aa22bd34f484b1a58013cca75d68687
Michael McKenzie <mckenzma@gmail.com>
2019-11-24 20:28:16 -0500

    delete commented out code

--- ui/src/BridgeSlider.js
+++ ui/src/BridgeSlider.js
@@ -1,4 +1,3 @@
-// import React from "react";
 import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
@@ -14,46 +13,10 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-// const marks = [
-//   {
-//     value: 0,
-//     label: "0°C"
-//   },
-//   {
-//     value: 20,
-//     label: "20°C"
-//   },
-//   {
-//     value: 37,
-//     label: "37°C"
-//   },
-//   {
-//     value: 100,
-//     label: "100°C"
-//   }
-// ];
-
-// function handleChange(event, value) {
-//     this.setState({ value: value});
-//     console.log(value);
-//     // this.value = value;
-//   }
-
 export default function DiscreteSlider({ sliderYears, selectedSliderYear }) {
   const classes = useStyles();
 
-  // this.state = {
-  //   value: Math.max(...sliderYears)
-  // };
-
   const [value, setValue] = useState(Math.max(...sliderYears));
-
-  // const [value,handleChange] = useState(0);
-  // const value = useState(Math.max(...sliderYears));
-
-  // const value = Math.max(...sliderYears);
-
-  // console.log(value);
 
   const marks = sliderYears.map(year => ({
     value: year,
@@ -63,15 +26,7 @@ export default function DiscreteSlider({ sliderYears, selectedSliderYear }) {
   const handleChange = (event, value) => {
     setValue(value);
     selectedSliderYear(value);
-    // console.log(value);
-
-    // this.value = value;
   };
-
-  // function handleDragStop() {
-  //   this.setState({ value: value });
-  //   // this.props.update(this.value);
-  // }
 
   return (
     <div className={classes.root}>
@@ -84,18 +39,12 @@ export default function DiscreteSlider({ sliderYears, selectedSliderYear }) {
         defaultValue={Math.max(...sliderYears)}
         // getAriaValueText={valuetext}
         aria-labelledby="discrete-slider-always"
-        // step={10}
         step={1}
         min={Math.min(...sliderYears)}
         max={Math.max(...sliderYears)}
-        // marks={marks}
         marks={sliderYears}
         valueLabelDisplay="on"
-        // trying to update state fron slider
         onChange={handleChange}
-        // onChange={() => this.setState({ value: this.state.value })}
-        // onDragStop={this.handleDragStop}
-        // onChange={(event, value) => this.handleChange(value)}
       />
     </div>
   );
